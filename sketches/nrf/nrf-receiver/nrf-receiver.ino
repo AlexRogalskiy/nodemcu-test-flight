c085dc490706046109fc1b89407da5cb663e8992
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-12-10 03:52:40 +0300

    c

--- sketches/nrf/nrf-receiver/nrf-receiver.ino
+++ sketches/nrf/nrf-receiver/nrf-receiver.ino
@@ -2,12 +2,17 @@
 #include <ESP8266WiFi.h>
 #include <RH_NRF24.h>
 #include <SPI.h>
+//#include "nRF24L01.h"
+#include "RF24.h"
 
 const char* ssid = "Pantera";
 const char* password = "YEzpe93r";
 
+int32_t data[5];
+
 int gatewayID = EEPROM.read(0);
 
+RF24 radio(99, 15);
 RH_NRF24 nrf24(2, 4); // use this for NodeMCU Amica/AdaFruit Huzzah ESP8266 Feather
 
 void setup()
@@ -32,10 +37,24 @@ void setup()
   nrf24.init();
   nrf24.setChannel(3);
   nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm);
+
+  radio.begin();
+  radio.setAutoAck(true);
+  radio.setChannel(100);
+  radio.setDataRate(RF24_2MBPS);   // скорость обмена RF24_2MBPS, RF24_1MBPS, RF24_250KBPS - максимальная чувствительность на 250 кбитс, NRF24L01 без плюса 250 не умеет
+  radio.setPALevel(RF24_PA_MAX);    // уровень мощности RF24_PA_MIN -18dBm, RF24_PA_LOW -12dBm, RF24_PA_HIGH -6dBM, RF24_PA_MAX 0dBm
+  radio.openReadingPipe(1, 0xAABBCCDD11LL);
+  radio.startListening();
 }
 
 void loop()
 {
+  if (radio.available()) {
+    radio.read( &data, sizeof(data));
+    Serial.println("Device ID: ");
+    Serial.print(data[0]);
+  }
+
   if (nrf24.available())
   {
     // Should be a message for us now
--- sketches/nrf/nrf-sender/nrf-sender.ino
+++ sketches/nrf/nrf-sender/nrf-sender.ino
@@ -1,6 +1,17 @@
 #include <EEPROM.h>
 #include <RH_NRF24.h>
 #include <SPI.h>
+#include "RF24.h"
+
+RF24 radio(9, 10);  // CE_PIN, CSN_PIN
+byte count = 0;     // счетчик количества просыпаний
+int32_t data[5];    // на данный момент используется int32_t чтобы иметь возможность получить большие числа счетчика передач, для тестирования продолжительности работы от батарейки,
+// потом можно обычный int
+// data 0 - данные АЦП или пересчитанное напряжение питания c тремя знаками(в милливольтах, на стороне приемника переменная float деленная на 1000
+// data 1 - счетчик отправленных пакетов, сбрасывается при снятии питания
+// data 2 - P атмсомферное в мм, можно умножить на 10 если нужны десятые, тогда на стороне приемника делить на 10 и float
+// data 3 - Т температура умноженное на 100 на стороне приемника float делить на 100
+// data 4 - H влажность умноженная на 100
 
 RH_NRF24 nrf24(2, 4); // use this for NodeMCU Amica/AdaFruit Huzzah ESP8266 Feather
 // RH_NRF24 nrf24(8, 7); // use this with Arduino UNO/Nano
@@ -26,6 +37,16 @@ void setup()
     Serial.println("setRF failed");
   }
   Serial.println("Transmitter started");
+
+  radio.begin();
+  radio.setAutoAck(true);                    // подтверждение приема, true - включено, false - отключено
+  radio.setRetries(2, 1);                 // количество попыток передачи если включен режим автоподтверждения приема, первая цифра - задержка между передачами, 1 = 250 мкс, вторая - количество попыток передачи
+  radio.setChannel(100);                     // номер канала, можно перед выбором просканировать эфир чтобы выяснить менее зашумленные каналы(использовать скетч scanner из библ. RF24)
+  radio.setDataRate(RF24_2MBPS);        // скорость обмена RF24_2MBPS, RF24_1MBPS, RF24_250KBPS - максимальная чувствительность на 250 кбитс
+  radio.setPALevel(RF24_PA_MAX);       // уровень мощности RF24_PA_MIN -18dBm, RF24_PA_LOW -12dBm, RF24_PA_HIGH -6dBM, RF24_PA_MAX 0dBm
+  radio.openWritingPipe(0xAABBCCDD11LL);    // уникальный номер передатчика, на приемнике должен быть такой же
+
+  //radio.powerDown();
 }
 
 void loop()
@@ -63,7 +84,20 @@ void loop()
   else
   {
     Serial.println("No reply.");
-
   }
+
+  radio_();
   delay(5000);
+}
+
+void radio_() {
+  data[0] = 0;
+  data[1] = 1;
+  data[2] = 2;
+  data[3] = 3;
+  data[4] = 4;
+
+  //radio.powerUp();
+  radio.write(&data, sizeof(data));
+  //radio.powerDown();
 }
