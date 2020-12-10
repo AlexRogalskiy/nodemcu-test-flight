5215c12c21e0257685e254824048f9ea787d170f
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-12-10 10:57:15 +0300

    c

--- sketches/nrf/nrf-receiver/nrf-receiver.ino
+++ sketches/nrf/nrf-receiver/nrf-receiver.ino
@@ -1,19 +1,25 @@
-#include <EEPROM.h>
+//#include <EEPROM.h>
 #include <ESP8266WiFi.h>
-#include <RH_NRF24.h>
 #include <SPI.h>
-//#include "nRF24L01.h"
+//#include <RH_NRF24.h>
+#include "nRF24L01.h"
 #include "RF24.h"
+
+char msg[6];
+//RF24 radio(7, 8);
+//RF24 radio(2, 15);
+RF24 radio(99, 4);
+//RF24 radio(2, 15);
+//RF24 radio(99, 15); // CE, CSN, 99 - это заглушка, чтобы освободить один пин, если передача не предполлагается то можно CE просто подтянуть резистором к питанию, иначе нужно указать вывод
+//RH_NRF24 nrf24(2, 4); // use this for NodeMCU Amica/AdaFruit Huzzah ESP8266 Feather
+const uint64_t pipe = 0xAABBCCDD11LL;
 
 const char* ssid = "Pantera";
 const char* password = "YEzpe93r";
 
 int32_t data[5];
 
-int gatewayID = EEPROM.read(0);
-
-RF24 radio(99, 15);
-RH_NRF24 nrf24(2, 4); // use this for NodeMCU Amica/AdaFruit Huzzah ESP8266 Feather
+//int gatewayID = EEPROM.read(0);
 
 void setup()
 {
@@ -21,7 +27,7 @@ void setup()
   Serial.print("Receiver Started, ID: ");
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  Serial.print(gatewayID);
+  //Serial.print(gatewayID);
   Serial.println();
 
   WiFi.begin(ssid, password);
@@ -34,50 +40,60 @@ void setup()
   Serial.println("");
   Serial.println("WiFi connected");
 
-  nrf24.init();
-  nrf24.setChannel(3);
-  nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm);
+  //  nrf24.init();
+  //  nrf24.setChannel(3);
+  //  nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm);
 
   radio.begin();
   radio.setAutoAck(true);
   radio.setChannel(100);
+  radio.setPayloadSize(7);
   radio.setDataRate(RF24_2MBPS);   // скорость обмена RF24_2MBPS, RF24_1MBPS, RF24_250KBPS - максимальная чувствительность на 250 кбитс, NRF24L01 без плюса 250 не умеет
   radio.setPALevel(RF24_PA_MAX);    // уровень мощности RF24_PA_MIN -18dBm, RF24_PA_LOW -12dBm, RF24_PA_HIGH -6dBM, RF24_PA_MAX 0dBm
-  radio.openReadingPipe(1, 0xAABBCCDD11LL);
+  //radio.openReadingPipe(1, 0xAABBCCDD11LL);
+  radio.openReadingPipe(1, pipe);
   radio.startListening();
 }
 
 void loop()
 {
+  Serial.println("Receiving data <<< ");
   if (radio.available()) {
-    radio.read( &data, sizeof(data));
+    //radio.read(&data, sizeof(data));
+    radio.read(msg, 6);
     Serial.println("Device ID: ");
-    Serial.print(data[0]);
-  }
-
-  if (nrf24.available())
-  {
-    // Should be a message for us now
-    uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
-    if (nrf24.recv(buf, &len))
-    {
-
-      // Send a reply
-      uint8_t sdata[] = "Data Received.";
-      nrf24.send(sdata, sizeof(sdata));
-      nrf24.waitPacketSent();
-
-      int deviceID = buf[0];
-
-      Serial.println("--- Data retrieved from device ---");
-      Serial.print("Device ID: ");
-      Serial.print(deviceID);
-    }
+    //Serial.print(data[0]);
+    //Serial.print(data[1]);
+    Serial.println(msg);
   }
   else
   {
     Serial.println("No New Message");
   }
+
+  //  if (nrf24.available())
+  //  {
+  //    // Should be a message for us now
+  //    uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+  //    uint8_t len = sizeof(buf);
+  //    if (nrf24.recv(buf, &len))
+  //    {
+  //
+  //      // Send a reply
+  //      uint8_t sdata[] = "Data Received.";
+  //      nrf24.send(sdata, sizeof(sdata));
+  //      nrf24.waitPacketSent();
+  //
+  //      int deviceID = buf[0];
+  //
+  //      Serial.println("--- Data retrieved from device ---");
+  //      Serial.print("Device ID: ");
+  //      Serial.print(deviceID);
+  //    }
+  //  }
+  //  else
+  //  {
+  //    Serial.println("No New Message");
+  //  }
   delay(2000);
 }
--- sketches/nrf/nrf-sender/nrf-sender.ino
+++ sketches/nrf/nrf-sender/nrf-sender.ino
@@ -1,10 +1,16 @@
-#include <EEPROM.h>
-#include <RH_NRF24.h>
+//#include <EEPROM.h>
 #include <SPI.h>
+//#include <RH_NRF24.h>
+#include "nRF24L01.h"
 #include "RF24.h"
 
-RF24 radio(9, 10);  // CE_PIN, CSN_PIN
-byte count = 0;     // счетчик количества просыпаний
+char msg[6] = "hello";
+//RF24 radio(2, 4);
+//RF24 radio(12, 14, 26, 25, 27);
+//RF24 radio(9, 10);  // CE_PIN, CSN_PIN
+RF24 radio(2, 4);
+const uint64_t pipe = 0xAABBCCDD11LL;
+
 int32_t data[5];    // на данный момент используется int32_t чтобы иметь возможность получить большие числа счетчика передач, для тестирования продолжительности работы от батарейки,
 // потом можно обычный int
 // data 0 - данные АЦП или пересчитанное напряжение питания c тремя знаками(в милливольтах, на стороне приемника переменная float деленная на 1000
@@ -13,10 +19,10 @@ int32_t data[5];    // на данный момент используется i
 // data 3 - Т температура умноженное на 100 на стороне приемника float делить на 100
 // data 4 - H влажность умноженная на 100
 
-RH_NRF24 nrf24(2, 4); // use this for NodeMCU Amica/AdaFruit Huzzah ESP8266 Feather
-// RH_NRF24 nrf24(8, 7); // use this with Arduino UNO/Nano
+//RH_NRF24 nrf24(2, 4); // use this for NodeMCU Amica/AdaFruit Huzzah ESP8266 Feather
+//RH_NRF24 nrf24(8, 7); // use this with Arduino UNO/Nano
 
-int deviceID = EEPROM.read(0);
+//int deviceID = EEPROM.read(0);
 
 void setup()
 {
@@ -24,80 +30,84 @@ void setup()
   Serial.setDebugOutput(true);
   delay(10);
 
-  if (!nrf24.init())
-  {
-    Serial.println("init failed");
-  }
-  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
-  if (!nrf24.setChannel(3))
-  {
-    Serial.println("setChannel failed");
-  }
-  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm)) {
-    Serial.println("setRF failed");
-  }
-  Serial.println("Transmitter started");
+  //  if (!nrf24.init())
+  //  {
+  //    Serial.println("init failed");
+  //  }
+  //  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
+  //  if (!nrf24.setChannel(3))
+  //  {
+  //    Serial.println("setChannel failed");
+  //  }
+  //  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm)) {
+  //    Serial.println("setRF failed");
+  //  }
+  //  Serial.println("Transmitter started");
 
   radio.begin();
   radio.setAutoAck(true);                    // подтверждение приема, true - включено, false - отключено
   radio.setRetries(2, 1);                 // количество попыток передачи если включен режим автоподтверждения приема, первая цифра - задержка между передачами, 1 = 250 мкс, вторая - количество попыток передачи
   radio.setChannel(100);                     // номер канала, можно перед выбором просканировать эфир чтобы выяснить менее зашумленные каналы(использовать скетч scanner из библ. RF24)
+  radio.setPayloadSize(7);
   radio.setDataRate(RF24_2MBPS);        // скорость обмена RF24_2MBPS, RF24_1MBPS, RF24_250KBPS - максимальная чувствительность на 250 кбитс
   radio.setPALevel(RF24_PA_MAX);       // уровень мощности RF24_PA_MIN -18dBm, RF24_PA_LOW -12dBm, RF24_PA_HIGH -6dBM, RF24_PA_MAX 0dBm
-  radio.openWritingPipe(0xAABBCCDD11LL);    // уникальный номер передатчика, на приемнике должен быть такой же
-
+  //radio.openWritingPipe(0xAABBCCDD11LL);    // уникальный номер передатчика, на приемнике должен быть такой же
+  radio.openWritingPipe(pipe);
   //radio.powerDown();
 }
 
 void loop()
 {
   Serial.println("Sending to gateway");
-  uint8_t data[4];
+  //  uint8_t data[4];
 
-  data[0] = deviceID;
+  //  data[0] = deviceID;
 
-  Serial.println("------------- Measurements -------------");
-  Serial.print(", ID: ");
-  Serial.print(data[0]);
-  Serial.println();
+  //  Serial.println("------------- Measurements -------------");
+  //  Serial.print(", ID: ");
+  //  Serial.print(data[0]);
+  //  Serial.println();
 
-  nrf24.send(data, sizeof(data));
-  nrf24.waitPacketSent();
+  //nrf24.send(data, sizeof(data));
+  //nrf24.waitPacketSent();
 
   // Now wait for a reply
-  uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+  //uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+  //uint8_t len = sizeof(buf);
 
-  if (nrf24.waitAvailableTimeout(1000))
-  {
-    // Should be a reply message for us now
-    if (nrf24.recv(buf, &len))
-    {
-      Serial.print("got reply: ");
-      Serial.println((char*)buf);
-    }
-    else
-    {
-      Serial.println("recv failed");
-    }
-  }
-  else
-  {
-    Serial.println("No reply.");
-  }
+  //  if (nrf24.waitAvailableTimeout(1000))
+  //  {
+  //    // Should be a reply message for us now
+  //    if (nrf24.recv(buf, &len))
+  //    {
+  //      Serial.print("got reply: ");
+  //      Serial.println((char*)buf);
+  //    }
+  //    else
+  //    {
+  //      Serial.println("recv failed");
+  //    }
+  //  }
+  //  else
+  //  {
+  //    Serial.println("No reply.");
+  //  }
 
-  radio_();
+  sendMessage();
   delay(5000);
 }
 
-void radio_() {
-  data[0] = 0;
+void sendMessage() {
+  int32_t data[5];
+  data[0] = 100;
   data[1] = 1;
   data[2] = 2;
   data[3] = 3;
-  data[4] = 4;
+  data[3] = 4;
 
   //radio.powerUp();
-  radio.write(&data, sizeof(data));
+  //radio.write(&data, sizeof(data));
+  Serial.println(msg);
+  radio.write(msg, 6);
   //radio.powerDown();
 }
